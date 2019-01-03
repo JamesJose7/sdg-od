d1429736ad05ca107844a9d7e2d3c9e715753064
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-03 10:00:12 -0500

    Refactor code for better readability

--- src/main/java/com/jeeps/ckan_extractor/controller/CkanExtractor.java
+++ src/main/java/com/jeeps/ckan_extractor/controller/CkanExtractor.java
@@ -16,41 +16,46 @@ public class CkanExtractor {
 
     private Gson mGson;
     private MysqlDatabase mDatabase;
+    private HttpService mHttpService;
+    private String mBaseUrl;
+    private String mListPackageDetailsUrl;
 
     public CkanExtractor() {
         mDatabase = new MysqlDatabase();
     }
 
     public void extract(String baseUrl) {
+        mBaseUrl = baseUrl;
+        mListPackageDetailsUrl = baseUrl + "package_show?id=";
         String listPackagesUrl = baseUrl + "package_list";
-        String listPackageDetailsUrl = baseUrl + "package_show?id=";
 
         mGson = new Gson();
-        HttpService httpService = new HttpService();
-        httpService.sendRequest(s -> {
-            System.out.println(s);
-            CkanContent ckanContent = mGson.fromJson(s, CkanContent.class);
-            System.out.println(ckanContent.getResult().length);
+        mHttpService = new HttpService();
+        mHttpService.sendRequest(this::extractDatasets, listPackagesUrl);
+    }
 
-            List<String> ckanDatasets = Arrays.asList(ckanContent.getResult());
-            ckanDatasets.forEach(dataset -> {
-                // Get details from dataset
-                httpService.sendRequest(result -> {
-                    JSONObject body = new JSONObject(result);
-                    // Dataset info
-                    JSONObject resultJson = body.getJSONObject("result");
-                    CkanPackage aPackage = mGson.fromJson(resultJson.toString(), CkanPackage.class);
-                    // Resource info
-                    JSONArray packageResource = resultJson.getJSONArray("resources");
-                    CkanResource[] resourcesCkan = mGson.fromJson(packageResource.toString(), CkanResource[].class);
-                    // Set origin URL
-                    aPackage.setOriginUrl(baseUrl);
-                    if (aPackage != null) {
-                        System.out.println(aPackage);
-                        mDatabase.savePackage(aPackage, resourcesCkan);
-                    }
-                }, (listPackageDetailsUrl + dataset));
-            });
-        }, listPackagesUrl);
+    private void extractDatasets(String json) {
+        System.out.println(json);
+        CkanContent ckanContent = mGson.fromJson(json, CkanContent.class);
+        System.out.println(ckanContent.getResult().length);
+
+        List<String> ckanDatasets = Arrays.asList(ckanContent.getResult());
+        ckanDatasets.forEach(dataset -> mHttpService.
+                        sendRequest(this::extractDatasetDetails, (mListPackageDetailsUrl + dataset)));
+    }
+
+    private void extractDatasetDetails(String result) {
+        // Get details from dataset
+        JSONObject body = new JSONObject(result);
+        // Dataset info
+        JSONObject resultJson = body.getJSONObject("result");
+        CkanPackage aPackage = mGson.fromJson(resultJson.toString(), CkanPackage.class);
+        // Resource info
+        JSONArray packageResource = resultJson.getJSONArray("resources");
+        CkanResource[] resourcesCkan = mGson.fromJson(packageResource.toString(), CkanResource[].class);
+        // Set origin URL
+        aPackage.setOriginUrl(mBaseUrl);
+        System.out.println(aPackage);
+        mDatabase.savePackage(aPackage, resourcesCkan);
     }
 }
