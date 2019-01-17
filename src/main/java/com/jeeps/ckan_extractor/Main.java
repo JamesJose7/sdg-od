61b8d9c966d476f7ea49f501a0658abd7c84037e
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-17 11:51:56 -0500

    Total execution time will be displayed when the process finishes

--- src/main/java/com/jeeps/ckan_extractor/Main.java
+++ src/main/java/com/jeeps/ckan_extractor/Main.java
@@ -2,9 +2,21 @@ package com.jeeps.ckan_extractor;
 
 import com.jeeps.ckan_extractor.controller.CkanExtractor;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
+        Instant start = Instant.now();
         CkanExtractor ckanExtractor = new CkanExtractor();
-        ckanExtractor.extract("http://ambar.utpl.edu.ec/api/action/");
+//        ckanExtractor.extract("http://ambar.utpl.edu.ec/api/action/");
+//        chowkanExtractor.extract("http://data.europa.eu/euodp/data/api/action/");
+        ckanExtractor.extractByPost("http://data.europa.eu/euodp/data/api/3/action/");
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.printf("Elapsed time:\n" +
+                "millis: %d\n" +
+                "seconds: %d\n", timeElapsed, (timeElapsed / 1000));
+//        ckanExtractor.extract("https://opendata.swiss/api/3/action/");
     }
 }
