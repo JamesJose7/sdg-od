93e12f1445ffea6a43e8ba4ed465a970cc6b598c
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-03-10 14:04:34 -0500

    Using jenna to generate triples from the extracted data with a custom vocabulary expanded from DCAT

--- src/main/java/com/jeeps/ckan_extractor/Main.java
+++ src/main/java/com/jeeps/ckan_extractor/Main.java
@@ -1,26 +1,46 @@
 package com.jeeps.ckan_extractor;
 
 import com.jeeps.ckan_extractor.controller.CkanExtractor;
-import com.jeeps.ckan_extractor.controller.SemanticCreator;
 
+import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.time.Instant;
 
+import static com.jeeps.ckan_extractor.controller.SemanticCreator.CURRENT_COUNTRY;
+import static com.jeeps.ckan_extractor.controller.SemanticCreator.CURRENT_PLATFORM;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Instant start = Instant.now();
         CkanExtractor ckanExtractor = new CkanExtractor();
-        //ckanExtractor.extract("http://ambar.utpl.edu.ec/api/action/");
-//        chowkanExtractor.extract("http://data.europa.eu/euodp/data/api/action/");
+
+//        CURRENT_COUNTRY = "Ecuador";
+//        CURRENT_PLATFORM = "Ambar UTPL";
+//        ckanExtractor.extract("http://ambar.utpl.edu.ec/api/action/");
+
+//        CURRENT_COUNTRY = "";
+//        CURRENT_PLATFORM = "EU Open Data Portal";
 //        ckanExtractor.extractByPost("http://data.europa.eu/euodp/data/api/3/action/");
+
+        /*CURRENT_COUNTRY = "Netherlands";
+        CURRENT_PLATFORM = "The Humanitarian Data Exchange";
+        ckanExtractor.extract("https://data.humdata.org/api/3/action/");*/
+
+        CURRENT_COUNTRY = "Switzerland";
+        CURRENT_PLATFORM = "Switzerland government";
+        ckanExtractor.extract("https://opendata.swiss/api/3/action/");
+
+//        CURRENT_COUNTRY = "Australia";
+//        CURRENT_PLATFORM = "Australian government";
+//        ckanExtractor.extract("https://data.gov.au/api/3/action/");
+
         Instant finish = Instant.now();
         long timeElapsed = Duration.between(start, finish).toMillis();
         System.out.printf("Elapsed time:\n" +
                 "millis: %d\n" +
                 "seconds: %d\n", timeElapsed, (timeElapsed / 1000));
-//        ckanExtractor.extract("https://opendata.swiss/api/3/action/");
 
-        SemanticCreator semanticCreator = new SemanticCreator();
-        semanticCreator.convertToDcat();
+//        SemanticCreator semanticCreator = new SemanticCreator();
+//        semanticCreator.convertToDcat();
     }
 }
--- src/main/java/com/jeeps/ckan_extractor/controller/SemanticCreator.java
+++ src/main/java/com/jeeps/ckan_extractor/controller/SemanticCreator.java
@@ -1,51 +1,185 @@
 package com.jeeps.ckan_extractor.controller;
 
-import com.jeeps.ckan_extractor.config.MysqlDatabase;
 import com.jeeps.ckan_extractor.model.CkanPackage;
 import com.jeeps.ckan_extractor.model.CkanResource;
-import com.jeeps.ckan_extractor.model.Dcat;
+import com.jeeps.ckan_extractor.vocabs.CKAN;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.vocabulary.DCAT;
+import org.apache.jena.vocabulary.DCTerms;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.SKOS;
 
-import java.io.*;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Arrays;
 
 public class SemanticCreator {
-    private MysqlDatabase mMysqlDatabase;
+    private Model mModel;
+    private final FileOutputStream os;
+    public static final String DATA_PREFIX = "http://example.org/data/";
 
-    public SemanticCreator() {
-        mMysqlDatabase = new MysqlDatabase(false);
+    public static String CURRENT_PLATFORM;
+    public static String CURRENT_COUNTRY;
+    private Property mDboCountryP;
+    private Resource mDboCountryC;
+    private String mDbo;
+    private String mDbr;
+
+    public SemanticCreator() throws FileNotFoundException {
+        // Create model
+        mModel = ModelFactory.createDefaultModel();
+        // File dump
+        File fos = new File("triples.rdf");
+        os = new FileOutputStream(fos);
+        initializeVocabs();
     }
 
-    public void convertToDcat() {
-        Map<CkanPackage, List<CkanResource>> datasets = mMysqlDatabase.retrieveDatasets();
-        Optional<Integer> sum = datasets.entrySet().stream()
-                .map(entry -> entry.getValue().size())
-                .reduce((integer, integer2) -> integer + integer2);
-        System.out.printf("Packages: %d\nResources: %d\n", datasets.size(), sum.orElse(0));
+    private void initializeVocabs() {
+        // Data prefix
+        mModel.setNsPrefix("data", DATA_PREFIX);
 
-        // Convert to dcat
-        Optional<String> completeDcat = datasets.entrySet().stream()
-                .map(this::datasetToDcat)
-                .reduce(String::concat);
-        //System.out.println(completeDcat);
+        // Foaf prefix
+        String foaf = FOAF.getURI();
+        mModel.setNsPrefix("foaf",foaf);
 
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream("dcat-representation.ttl"), "utf-8"))) {
-            writer.write(Dcat.NAMESPACES + completeDcat.orElse(""));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Dcat prefix
+        String dcat = DCAT.getURI();
+        mModel.setNsPrefix("dcat", dcat);
+
+        // DCTerms prefix
+        String dct = DCTerms.getURI();
+        mModel.setNsPrefix("dct", dct);
+
+        // SKOS prefix
+        String skos = SKOS.getURI();
+        mModel.setNsPrefix("skos", skos);
+
+        // DBO prefix
+        mDbo = "http://dbpedia.org/ontology/";
+        mDbr = "http://dbpedia.org/resource/";
+        mModel.setNsPrefix("dbo", mDbo);
+        mModel.setNsPrefix("dbr", mDbr);
+        mDboCountryP = ResourceFactory.createProperty(mDbo + "country");
+        mDboCountryC = ResourceFactory.createResource(mDbo + "Country");
+
+        // Our Ontology prefix
+        String vocabPrefix = CKAN.getURI();
+        mModel.setNsPrefix("ckan",vocabPrefix);
     }
 
-    private String datasetToDcat(Map.Entry<CkanPackage, List<CkanResource>> dataset) {
-        return new Dcat.DcatBuilder(dataset.getKey())
-                .withResources(dataset.getValue())
-                .build()
-                .getTurtleRepresentation();
+    public void generateTriples(CkanPackage aPackage, CkanResource[] resourcesCkan) {
+        // Create package as Catalog
+        Resource catalog = mModel.createResource(DATA_PREFIX + urlify(aPackage.getName()))
+                .addProperty(RDF.type, CKAN.CkanCatalog);
+
+        if (exists(aPackage.getAuthor()))
+            catalog.addProperty(DCTerms.publisher, mModel.createResource(DATA_PREFIX + urlify(aPackage.getName() + "_publisher"))
+                    .addProperty(RDF.type, FOAF.Agent)
+                    .addProperty(FOAF.name, aPackage.getAuthor()))
+                    .addProperty(CKAN.isPrivate, Boolean.toString(aPackage.isPrivate()));
+        if (exists(aPackage.getIssued()))
+            catalog.addProperty(DCTerms.issued, aPackage.getIssued());
+        if (exists(aPackage.getVersion()))
+            catalog.addProperty(CKAN.version, aPackage.getVersion());
+        if (exists(aPackage.getTitle()))
+            catalog.addProperty(DCTerms.title, aPackage.getTitle());
+        if (exists(aPackage.getDescription()))
+            catalog.addProperty(DCTerms.description, aPackage.getDescription());
+        if (exists(aPackage.getState()))
+            catalog.addProperty(CKAN.status, aPackage.getState());
+        if (exists(aPackage.getModified()))
+            catalog.addProperty(DCTerms.modified, aPackage.getModified());
+        if (exists(aPackage.getLicense_title()))
+            catalog.addProperty(CKAN.license, aPackage.getLicense_title());
+
+        // Add Government
+        catalog.addProperty(CKAN.provenance, mModel.createResource(DATA_PREFIX + urlify(CURRENT_PLATFORM))
+                .addProperty(RDF.type, CKAN.CkanPlatform)
+                .addProperty(mDboCountryP, mModel.createResource(mDbr + CURRENT_COUNTRY)
+                        .addProperty(RDF.type, mDboCountryC.getURI())));
+
+        // Add Groups
+        aPackage.getGroups().forEach(tag -> {
+            String groupName = tag.getAsJsonObject().get("display_name").getAsString();
+            catalog.addProperty(CKAN.group, mModel.createResource(DATA_PREFIX + urlify(groupName))
+                    .addProperty(RDF.type, CKAN.Group)
+                    .addProperty(CKAN.title, groupName));
+        });
+
+        // Add Tags
+        aPackage.getTags().forEach(tag -> {
+            String tagName = tag.getAsJsonObject().get("display_name").getAsString();
+            catalog.addProperty(CKAN.tag, mModel.createResource(DATA_PREFIX + urlify(tagName))
+                    .addProperty(RDF.type, CKAN.Tag)
+                    .addProperty(SKOS.prefLabel, tagName));
+        });
+
+        // Add Organization
+        String orgName = aPackage.getOrganization().has("title") ? aPackage.getOrganization().get("title").getAsString() : "org_" + urlify(aPackage.getName());
+        String orgStatus = aPackage.getOrganization().has("state") ? aPackage.getOrganization().get("state").getAsString() : "";
+        String orgPoliticalLevel = aPackage.getOrganization().has("political_level") ? aPackage.getOrganization().get("political_level").getAsString() : "";
+        Resource organization = mModel.createResource(DATA_PREFIX + urlify(orgName))
+                .addProperty(RDF.type, CKAN.Organization)
+                .addProperty(FOAF.name, orgName);
+        if (exists(orgStatus))
+            organization.addProperty(CKAN.status, orgStatus);
+        if (exists(orgPoliticalLevel))
+            organization.addProperty(CKAN.politicalLevel, orgPoliticalLevel);
+        catalog.addProperty(CKAN.organization, organization);
+
+        // Add resources
+        Arrays.stream(resourcesCkan)
+                .forEach(resource -> {
+                    // Dataset
+                    Resource dataset = mModel.createResource(DATA_PREFIX + urlify(resource.getName()))
+                            .addProperty(RDF.type, CKAN.CkanDataset)
+                            .addProperty(DCTerms.title, resource.getName());
+                    if (exists(resource.getLicense()))
+                        dataset.addProperty(CKAN.license, resource.getLicense());
+                    if (exists(resource.getDescription()))
+                        dataset.addProperty(DCTerms.description, resource.getDescription());
+                    if (exists(resource.getCreated()))
+                        dataset.addProperty(DCTerms.issued, resource.getCreated());
+                    if (exists(resource.getModified()))
+                        dataset.addProperty(DCTerms.modified, resource.getModified());
+                    if (exists(resource.getState()))
+                        dataset.addProperty(CKAN.status, resource.getState());
+                    if (exists(resource.getFormat()))
+                        dataset.addProperty(CKAN.mediaType, resource.getFormat());
+
+                    // Distribution
+                    Resource distribution = mModel.createResource(DATA_PREFIX + "dist_" + urlify(resource.getName()))
+                            .addProperty(RDF.type, DCAT.Distribution)
+                            .addProperty(DCAT.downloadURL, resource.getUrl());
+                    if (exists(resource.getByteSize()))
+                        distribution.addProperty(DCAT.byteSize, resource.getByteSize());
+
+                    // Add relations
+                    dataset.addProperty(DCAT.distribution, distribution);
+                    catalog.addProperty(DCAT.dataset, dataset);
+                });
+    }
+
+    public void writeRdfFile() {
+        // Write model to file
+        RDFWriter writer = mModel.getWriter("RDF/XML");
+        writer.write(mModel, os,  "");
+    }
+
+    public String urlify(String string) {
+        return string.trim().toLowerCase()
+                .replaceAll(" ", "_")
+                .replaceAll("_", "_")
+                .replaceAll("\"", "")
+                .replaceAll("\\.", "_")
+                .replaceAll("\n", "_");
+    }
+
+    private boolean exists(String string) {
+        if (string == null)
+            return false;
+        return !string.isBlank() && !string.isEmpty();
     }
 }
