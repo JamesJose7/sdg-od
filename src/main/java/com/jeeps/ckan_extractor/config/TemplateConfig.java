3eb4693785fe96b387ec1c652831d216937b6fd2
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-04-16 12:41:18 -0500

    Initial web layout mockup

---
+++ src/main/java/com/jeeps/ckan_extractor/config/TemplateConfig.java
@@ -0,0 +1,35 @@
+package com.jeeps.ckan_extractor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class TemplateConfig {
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCacheable(false);
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+/*
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        final SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.addTemplateResolver(templateResolver());
+        springTemplateEngine.addDialect(new SpringSecurityDialect());
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        final ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        viewResolver.setCharacterEncoding("UTF-8");
+        return viewResolver;
+    }*/
+}
