830cf4bc677f34a153faaf60be5c7fc021a58579
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-04-28 22:17:21 -0500

    Bug fix: loading the rdf transformer with no datasets returned a thymeleaf exception

--- src/main/java/com/jeeps/ckan_extractor/web/controller/AdminController.java
+++ src/main/java/com/jeeps/ckan_extractor/web/controller/AdminController.java
@@ -1,17 +1,12 @@
 package com.jeeps.ckan_extractor.web.controller;
 
 import com.jeeps.ckan_extractor.service.CkanPackageService;
-import com.jeeps.ckan_extractor.web.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -43,31 +38,5 @@ public class AdminController {
         model.addAttribute("urlCount", ckanPackageCount);
         /*model.addAttribute("resourcesCount", resourcesCount);*/
         return "admin";
-    }
-
-    @RequestMapping(value = "/admin/extract", method = RequestMethod.POST)
-    public String extractCkanData(@RequestParam(value = "ckanURL", required = false) String[] ckanUrls,
-                                  RedirectAttributes redirectAttributes) {
-        if (ckanUrls == null) {
-            // At least one checkbox should be selected
-            redirectAttributes.addFlashAttribute("flash",
-                    new FlashMessage("Please select at least one repository", FlashMessage.Status.FAILURE));
-            return "redirect:/admin";
-        }
-        Arrays.stream(ckanUrls).forEach(System.out::println);
-
-        /*Collection<CkanPackage> ckanPackages = ckanPackageService.findAllByOriginUrl(ckanUrls[0]);
-
-        CompletableFuture<Long> task = semanticCreatorService.generateCkanTriples(ckanPackages);
-        task.whenComplete((aLong, throwable) -> System.out.println("The result is: " + aLong));
-        CompletableFuture.allOf(task).join();
-        try {
-            semanticCreatorService.writeFile("testTriples");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("What what");*/
-
-        return "redirect:/admin";
     }
 }
