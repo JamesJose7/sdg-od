a8f8f9d91ad879da91022682b2f320b991972273
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-05-05 00:52:21 -0500

    Ckan packages and resource services are now transactional

--- src/main/java/com/jeeps/ckan_extractor/service/CkanPackageServiceImpl.java
+++ src/main/java/com/jeeps/ckan_extractor/service/CkanPackageServiceImpl.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
 
 @Service
+@Transactional
 public class CkanPackageServiceImpl implements CkanPackageService {
     @Autowired
     private CkanPackageDao ckanPackageDao;
--- src/main/java/com/jeeps/ckan_extractor/service/CkanResourceServiceImpl.java
+++ src/main/java/com/jeeps/ckan_extractor/service/CkanResourceServiceImpl.java
@@ -4,8 +4,10 @@ import com.jeeps.ckan_extractor.dao.CkanResourceDao;
 import com.jeeps.ckan_extractor.model.CkanResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CkanResourceServiceImpl implements CkanResourceService {
     @Autowired
     private CkanResourceDao ckanResourceDao;
