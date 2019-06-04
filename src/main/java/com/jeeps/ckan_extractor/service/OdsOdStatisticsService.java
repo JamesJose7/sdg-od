3efcdc0075596a4c2018de800c75881142bf3e4e
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-06-03 23:44:53 -0500

    Added a pie graph to show how many resources are related to each SDG

---
+++ src/main/java/com/jeeps/ckan_extractor/service/OdsOdStatisticsService.java
@@ -0,0 +1,7 @@
+package com.jeeps.ckan_extractor.service;
+
+import java.util.Map;
+
+public interface OdsOdStatisticsService {
+    Map<String, Integer> howManyDatasetsRelateToEachGoal();
+}
--- src/main/java/com/jeeps/ckan_extractor/web/controller/OdsController.java
+++ src/main/java/com/jeeps/ckan_extractor/web/controller/OdsController.java
@@ -1,14 +1,29 @@
 package com.jeeps.ckan_extractor.web.controller;
 
+import com.jeeps.ckan_extractor.service.OdsOdStatisticsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 @Controller
 public class OdsController {
+    @Autowired
+    private OdsOdStatisticsService odsOdStatisticsService;
 
     @RequestMapping("/ods/overview")
     public String odsOverview(Model model) {
+        try {
+            Map<String, Integer> datasetsPerOds = odsOdStatisticsService.howManyDatasetsRelateToEachGoal();
+            model.addAttribute("odsLabels", datasetsPerOds.keySet());
+            model.addAttribute("datasetsCounts", datasetsPerOds.values());
+        } catch (Exception e) {
+            model.addAttribute("odsLabels", new ArrayList<String>());
+            model.addAttribute("datasetsCounts", new ArrayList<Integer>());
+        }
         return "/ods/ods-od-overview";
     }
 }
