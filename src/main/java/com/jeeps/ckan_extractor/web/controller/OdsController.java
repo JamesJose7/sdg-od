e7570e807e6abe1e323e9db032c3279ee3b8c79d
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-06-05 13:21:15 -0500

    Using data tables to show the list of sdg related data sets with server side processing

--- src/main/java/com/jeeps/ckan_extractor/web/controller/OdsController.java
+++ src/main/java/com/jeeps/ckan_extractor/web/controller/OdsController.java
@@ -1,31 +1,41 @@
 package com.jeeps.ckan_extractor.web.controller;
 
-import com.jeeps.ckan_extractor.core.OdsOdLinker;
-import com.jeeps.ckan_extractor.service.OdsOdStatisticsService;
+import com.jeeps.ckan_extractor.model.SdgRelatedDataset;
+import com.jeeps.ckan_extractor.service.KnowledgeBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import static com.jeeps.ckan_extractor.dao.KnowledgeBaseDaoImpl.SPARQL_ENDPOINT;
 
 @Controller
 public class OdsController {
     @Autowired
-    private OdsOdStatisticsService odsOdStatisticsService;
+    private KnowledgeBaseService knowledgeBaseService;
 
     @RequestMapping("/sdg/overview")
     public String odsOverview(Model model) {
-        model.addAttribute("sparqlEndpoint", OdsOdLinker.SPARQL_ENDPOINT);
+        model.addAttribute("sparqlEndpoint", SPARQL_ENDPOINT);
         try {
-            Map<String, Integer> datasetsPerOds = odsOdStatisticsService.howManyDatasetsRelateToEachGoal();
+            Map<String, Integer> datasetsPerOds = knowledgeBaseService.howManyDatasetsRelateToEachGoal();
             model.addAttribute("odsLabels", datasetsPerOds.keySet());
             model.addAttribute("datasetsCounts", datasetsPerOds.values());
         } catch (Exception e) {
             model.addAttribute("odsLabels", new ArrayList<String>());
             model.addAttribute("datasetsCounts", new ArrayList<Integer>());
         }
-        return "/ods/ods-od-overview";
+        return "ods/ods-od-overview";
+    }
+
+    @RequestMapping("/sdg/datasets")
+    public String datasetsRelatedToOds(Model model) {
+        List<SdgRelatedDataset> datasets = knowledgeBaseService.findAllCatalogsRelatedToOds();
+        model.addAttribute("datasets", datasets);
+        return "ods/datasets-list";
     }
 }
